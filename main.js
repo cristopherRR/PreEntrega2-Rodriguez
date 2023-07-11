bd0c7f6d2ba9e14ce151ee9d416f2bcc6c5ca5e3
cristopherRR <cristohper.rr@gmail.com>
2023-07-10 20:17:50 -0400

    correcion de preentrega

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 
-
-
+let porcentaje = 0.30;
+let precio = 0;
 
 function reducirDescuento () {
+
     precio = prompt("Ingresar precio del producto");
 	cantidad = prompt("Ingresar cantidad");
 
-	    compra = precio * cantidad;
-	    descuento = compra * 0.30;
-	    pagar = compra - descuento;
+    compra = precio * cantidad;
+    descuento = compra * porcentaje;
+    pagar = compra - descuento;
 
-	console.log("El desceunto es: " + descuento + "$");
-	console.log("El total a pagar es: " + pagar + "$");
+	console.log("El descuento es: " + descuento + "$");
+	alert("El total a pagar es: " + pagar + "$");
 }
+
+
 
 function cobrarPago (){
     monto = prompt("ingrese monto a pagar");
-    
 
     if(monto >= pagar){ 
     vuelto = monto - pagar;
@@ -27,6 +29,9 @@ function cobrarPago (){
 }
 
 }
-
-reducirDescuento ();
+while(precio != "ESC" ) {
+    reducirDescuento ();
 cobrarPago ();
+
+precio = prompt("desea seguir comprando");
+}
