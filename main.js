c7cda432b4af60dab4b28bbfb97ad759a5886970
cristopherRR <cristohper.rr@gmail.com>
2023-07-26 14:19:36 -0400

    correccion

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-function productos(id, producto, talla, stock, precio) {
+function productos(id, producto, stock, precio) {
     this.id = id
     this.producto = producto
-    this.talla = talla
     this.stock = stock
     this.cantidad = 0
     this.precio = precio
 }
 
-const prenda = new productos(0, "blusa", "s", 4, 10)
-const prenda1 = new productos(1, "pantalon marron", "28", 5, 25)
-const prenda2 = new productos(2, "short", "28", 2, 20)
-const prenda3 = new productos(3, "zapato", "36", 7, 35)
-const prenda4 = new productos(4, "vestido", "m", 5, 20)
-const prenda5 = new productos(5, "top", "s", 5, 10)
-const prenda6 = new productos(6, "lentes", "0", 3, 12)
-const prenda7 = new productos(7, "collar", "0", 8, 25)
-const prenda8 = new productos(8, "pantalon negro", "38", 5, 25)
-const prenda9 = new productos(9, "pantalon gris", "38", 3, 25)
-const prenda10 = new productos(10, "blusa negra", "s", 2, 25)
+const prenda = new productos(0, "blusa", 4, 10)
+const prenda1 = new productos(1, "pantalon marron", 5, 25)
+const prenda2 = new productos(2, "short", 2, 20)
+const prenda3 = new productos(3, "zapato", 7, 35)
+const prenda4 = new productos(4, "vestido", 5, 20)
+const prenda5 = new productos(5, "top", 5, 10)
+const prenda6 = new productos(6, "lentes", 3, 12)
+const prenda7 = new productos(7, "collar", 8, 25)
+const prenda8 = new productos(8, "pantalon negro", 5, 25)
+const prenda9 = new productos(9, "pantalon gris", 3, 25)
+const prenda10 = new productos(10, "blusa negra", 2, 25)
 
 let prods = [prenda, prenda1, prenda2, prenda3, prenda4, prenda5, prenda6, prenda7, prenda8, prenda9, prenda10]
 
