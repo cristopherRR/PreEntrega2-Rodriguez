e26023d02a688847c7871c7b416a28e315be6f73
Cristopher Javier de la Cruz Rodriguez Rivas <30538355+cristopherRR@users.noreply.github.com>
2023-07-31 08:33:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function obtenerProductosSeleccionados(productosDisponibles) {
 		let nombreProducto;
 		prods.forEach((prod,index) => nombreProducto += `
 		${index} ${prod.producto}`)
-		let eleccion = parseInt(prompt("Seleccione un articulo: " + nombreProducto))
+		let eleccion = parseInt(prompt("Seleccione un articulo: " + producto))
 
 		let existeEnElCarrito = carrito.find(prod => prod.id === eleccion)
 
